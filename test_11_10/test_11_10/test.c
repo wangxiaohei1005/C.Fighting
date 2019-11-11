c6d2480766201ff2a136ce42684efb1576c78ed8
wangxiaohei1005 <965896331@qq.com>
2019-11-11 21:25:15 +0800

    后续函数理解

--- test_11_10/test_11_10/test.c
+++ test_11_10/test_11_10/test.c
@@ -43,40 +43,86 @@
 //}
 
 
-int main()
+//int main()
+//{
+//	char str[] = "This is a simple string";
+//	char * pch;
+//	pch = strstr(str, "simple");
+//	strncpy(pch, "sample", 6);
+//	puts(str);
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	char str[] = "- This, a sample string.";
+//	char * pch;
+//	printf("Splitting string \"%s\" into tokens:\n", str);
+//	pch = strtok(str, " ,.-");
+//	while (pch != NULL)
+//	{
+//		printf("%s\n", pch);
+//		pch = strtok(NULL, " ,.-");
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char *p = "zhangpengwei@bitedu.tech";
+//	const char* sep = ".@";
+//	char arr[30];
+//	char *str = NULL;
+//	strcpy(arr, p);//将数据拷贝一份，处理arr数组的内容
+//	for (str = strtok(arr, sep); str != NULL; str = strtok(NULL, sep))
+//	{
+//		printf("%s\n", str);
+//	}
+//}
+
+#include <assert.h>
+int my_strlen(const char* str)//函数strlen
 {
-	char str[] = "This is a simple string";
-	char * pch;
-	pch = strstr(str, "simple");
-	strncpy(pch, "sample", 6);
-	puts(str);
-	return 0;
+	assert(str != NULL);
+	int len = 0;
+	while (*str != '\0')
+	{
+		++len;
+		++str;
+	}
+	return len;
+}
+//int main()
+//{
+//	char p[] = "hello world" ;
+//	printf("%d\n", my_strlen(p));
+//	printf("%d\n", sizeof(p));
+//}
+
+char* my_strcpy(char* dst, const char* src)
+{
+	assert(src != NULL);
+	assert(dst != NULL);
+	char* ret = dst;
+	/*while (*src)
+	{
+		*dst = *src;
+		++src;
+		++dst;
+	}
+	*dst = '\0';
+	return ret;*/
+	//库函数：
+	while (*dst++ = *src++)
+		return ret;
 }
 
-
 int main()
 {
-	char str[] = "- This, a sample string.";
-	char * pch;
-	printf("Splitting string \"%s\" into tokens:\n", str);
-	pch = strtok(str, " ,.-");
-	while (pch != NULL)
-	{
-		printf("%s\n", pch);
-		pch = strtok(NULL, " ,.-");
-	}
+	char p1[] = "hello111";
+	char p2[20];
+	strcpy(p2, p1);
+	printf("%s\n", p2);
 	return 0;
-}
-
-int main()
-{
-	char *p = "zhangpengwei@bitedu.tech";
-	const char* sep = ".@";
-	char arr[30];
-	char *str = NULL;
-	strcpy(arr, p);//将数据拷贝一份，处理arr数组的内容
-	for (str = strtok(arr, sep); str != NULL; str = strtok(NULL, sep))
-	{
-		printf("%s\n", str);
-	}
 }
