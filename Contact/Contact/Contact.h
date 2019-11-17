bd1137aa736eb3d18f4390c084cb1bfa9c87aecc
wangxiaohei1005 <965896331@qq.com>
2019-11-17 19:23:44 +0800

    通讯录实现

--- Contact/Contact/Contact.h
+++ Contact/Contact/Contact.h
@@ -5,6 +5,7 @@
 #define TELE 12
 #define ADDR 200
 #define MAX 200//总存储量
+#define DEFAULT 1
 typedef struct info
 {
 	char name[NAME];
@@ -16,17 +17,27 @@ typedef struct info
 
 }info;
 
-typedef struct Contact
+//typedef struct Contact
+//{
+//	info data[MAX];
+//	int size;
+//}Contact, *pContact;
+
+typedef struct Contact//内存动态存储
 {
-	info data[MAX];
+	info* data;
 	int size;
-}Contact, *pContact;
+	int capacity;
+}Contact,*pContact;
 
 void InitContact(pContact pc);//初始化
 void AddContact(pContact pc);//添加信息
 void DelContact(pContact pc);//删除
 void SearchContact(pContact pc);//查找
-int FindContact(pContact，char* name);//寻找
+int FindContact(pContact ,char* name);//寻找
 void ModifyContact(pContact pc);//更改
 void SortContact(pContact pc);//排序
 void ShowContact(pContact pc);//打印
+int CheckCapacity(pContact pc);
+void LoadContact(pContact pc);
+void SaveContact(pContact pc);
--- Contact/Contact/test.c
+++ Contact/Contact/test.c
@@ -1,16 +1,77 @@
 #include "Contact.h"
-
-void test()
+#include <stdio.h>
+#include <string.h>
+enum OPTION
 {
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MOD,
+	SHOW,
+	SORT,
+	SAVE,
+	LOAD,
+};
+
+void menu()
+{
+	printf("***************************************************************\n");
+	printf("********************  0.exit     1.add     ********************\n");
+	printf("********************  2.del      3.search  ********************\n");
+	printf("********************  4.modify   5.show    ********************\n");
+	printf("********************  6.sort     7.load    ********************\n");
+	printf("********************  8.save               ********************\n");
+	printf("***************************************************************\n");
+}
+ 
+void testContact()
+{
+	int option;
 	Contact ct;
 	InitContact(&ct);
-	AddContact(&ct);
-	DelContact(&ct);
-	SearchContact(&ct);
+	do
+	{
+		menu();
+		scanf("%d", &option);
+		switch (option)
+		{
+		case EXIT:
+			printf("您好，系统推出成功！！！\n");
+			break;
+		case ADD:
+			AddContact(&ct);
+			break;
+		case DEL:
+			DelContact(&ct);
+			break;
+		case SEARCH:
+			SearchContact(&ct);
+			break;
+		case MOD:
+			ModifyContact(&ct);
+			break;
+		case SHOW:
+			ShowContact(&ct);
+			break;
+		case SORT:
+			SortContact(&ct);
+			break;
+		case LOAD:
+			LoadContact(&ct);
+			break;
+		case SAVE:
+			SaveContact(&ct);
+			break;
+		default:
+			printf("请注意，输入的选项有误！！！请重新输入！！！\n");
+		}
+	} while (option);
+	
 }
 
 int main()
 {
-	test();
+	testContact();
 	return 0;
 }
