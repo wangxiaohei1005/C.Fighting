3e94401d88bd1ac6a78c215587f2d76d4296184d
wangxiaohei1005 <18309253051@sina.cn>
2019-12-01 22:30:40 +0800

    1

--- test(数据结构顺序表题解)/test(数据结构顺序表题解)/test.c
+++ test(数据结构顺序表题解)/test(数据结构顺序表题解)/test.c
@@ -1,79 +1,106 @@
 #include <stdio.h>
+//
+//void Func1(int N) 
+//{
+//	int count = 0;
+//	for (int i = 0; i < N; ++i) 
+//	{
+//		for (int j = 0; j < N; ++j)
+//		{
+//			++count;//此处时间复杂度应该为n^2
+//		}
+//	}
+//	for (int k = 0; k < 2 * N; ++k) 
+//	{
+//		++count;
+//	}
+//	int M = 10;
+//	while (M--) 
+//	{
+//		++count;
+//	}
+//	printf("%d\n", count);
+//}
+//
+////int main()
+////{
+////	int N = 7;
+////	Func1(N);
+////	return 0;
+////}
+//
+//void Func2(int N) 
+//{
+//	int count = 0;
+//	for (int k = 0; k < 2 * N; ++k) 
+//	{
+//		++count;
+//	}
+//	int M = 10;
+//	while (M--) 
+//	{
+//		++count;
+//	}
+//	printf("%d\n", count);
+//}
+//
+//
+////int main()
+////{
+////	int N = 7;
+////	Func2(N);
+////	return 0;
+////}
+//
+//void Func3(int N, int M) 
+//{
+//	int count = 0;
+//	for (int k = 0; k < M; ++k) 
+//	{
+//		++count;
+//	}
+//	for (int k = 0; k < N; ++k) 
+//	{
+//		++count;
+//	}
+//	printf("%d\n", count);
+//}
 
-void Func1(int N) 
+//int main()
+//{
+//	int N = 7, M = 6;
+//	Func3(N, M);
+//	return 0;
+//}
+//
+//long long Factorial(size_t N) 
+//{
+//	return N < 2 ? N : Factorial(N - 1)*N;
+//}
+int removeElement(int* nums, int numsSize, int val)
 {
-	int count = 0;
-	for (int i = 0; i < N; ++i) 
+	int src = 0;
+	int dst = 0;
+	while (numsSize > src)
 	{
-		for (int j = 0; j < N; ++j)
+		if (nums[src] != val)
 		{
-			++count;//此处时间复杂度应该为n^2
+			nums[dst] = nums[src];
+			src++;
+			dst++;
+		}
+		else
+		{
+			src++;
+		}
+		if (nums[dst] == NULL && nums[src] )
+		{
+			return 0;
+		}
+		else
+		{
+			return nums[dst];
 		}
 	}
-	for (int k = 0; k < 2 * N; ++k) 
-	{
-		++count;
-	}
-	int M = 10;
-	while (M--) 
-	{
-		++count;
-	}
-	printf("%d\n", count);
-}
-
-//int main()
-//{
-//	int N = 7;
-//	Func1(N);
-//	return 0;
-//}
-
-void Func2(int N) 
-{
-	int count = 0;
-	for (int k = 0; k < 2 * N; ++k) 
-	{
-		++count;
-	}
-	int M = 10;
-	while (M--) 
-	{
-		++count;
-	}
-	printf("%d\n", count);
-}
-
-
-//int main()
-//{
-//	int N = 7;
-//	Func2(N);
-//	return 0;
-//}
-
-void Func3(int N, int M) 
-{
-	int count = 0;
-	for (int k = 0; k < M; ++k) 
-	{
-		++count;
-	}
-	for (int k = 0; k < N; ++k) 
-	{
-		++count;
-	}
-	printf("%d\n", count);
-}
-
-int main()
-{
-	int N = 7, M = 6;
-	Func3(N, M);
-	return 0;
-}
-
-long long Factorial(size_t N) 
-{
-	return N < 2 ? N : Factorial(N - 1)*N;
+	
 }
