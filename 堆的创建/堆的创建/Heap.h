75c33bcc1e94c74047a5a6bd06931e16b6185740
wangxiaohei1005 <18309253051@sina.cn>
2020-03-04 20:45:17 +0800

    代码更新

--- 堆的创建/堆的创建/Heap.h
+++ 堆的创建/堆的创建/Heap.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
 
+// Ð¡¶Ñ
 typedef int HpDataType;
 typedef struct Heap
 {
@@ -9,8 +12,15 @@ typedef struct Heap
 	size_t      _capacity;
 }Heap;
 
-Heap* HeapCreate(HpDataType* a, size_t n);
+void AdjustDown(HpDataType* a, size_t n, int root);
+
+void HeapCreate(Heap* hp, HpDataType* a, size_t n);
 void HeapPush(Heap* hp, HpDataType x);
 void HeapPop(Heap* hp);
 HpDataType HeapTop(Heap* hp);
+int HeapEmpty(Heap* hp);
+
 void HeapPrint(Heap* hp);
+
+
+// topK
