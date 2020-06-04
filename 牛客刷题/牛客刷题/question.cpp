6bdd8122c39aecc0076793cccf83b6fe04b254e0
wangxiaohei1005 <18309253051@sina.cn>
2020-06-04 23:57:54 +0800

    1

--- 牛客刷题/牛客刷题/question.cpp
+++ 牛客刷题/牛客刷题/question.cpp
@@ -108,11 +108,11 @@ int main()
 }*/
 
 //2.
-
-#include<iostream>
-#include<string>
-
-using namespace std;
+//
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
 
 //int main()
 //{
@@ -128,27 +128,142 @@ using namespace std;
 //}
 
 
+//int main()
+//{
+//	string str;
+//	getline(cin, str);
+//	string tmp, res;
+//	for (int i = 0; i < str.size(); ++i)
+//	{
+//		if (str[i] == ' ')
+//		{
+//			res = ' ' + tmp + res;
+//			tmp = "\0";
+//		}
+//		else
+//		{
+//			tmp += str[i];
+//		}
+//	}
+//	if (tmp.size())
+//	{
+//		res = tmp + res;
+//	}
+//	cout << res << endl;
+//	return 0;
+//}
+
+
+//6.3
+//1.
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	string str,end;
+//	cin >> str;
+//	int i = 0, max_len = 0;
+//	int strlen = str.length();
+//	while (i < strlen)
+//	{
+//		if (str[i] < '0' || str[i] > '9')
+//		{
+//			++ i;
+//		}
+//		else
+//		{
+//			string tmp = "";
+//			int len = 0;
+//			while (i < strlen && str[i] <= '9' && str[i] >= '0')
+//			{
+//				tmp = tmp + str[i];
+//				++i;
+//				++len;
+//			}
+//			if (len > max_len)
+//			{
+//				max_len = len;
+//				end = tmp;
+//			}
+//		}
+//	}
+//	cout << end << endl;
+//	return 0;
+//}
+
+//6.3.2
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//class Solution {
+//public:
+//	int MoreThanHalfNum_Solution(vector<int> numbers) 
+//	{
+//		if (numbers.empty())
+//			return 0;
+//		sort(numbers.begin(), numbers.end());
+//
+//		int ques = numbers[numbers.size() / 2];//如果出现次数超过数组个数一半，则其数组中间位置一定为此数
+//
+//		int count = 0;
+//		for (int i = 0; i < numbers.size(); ++i)
+//		{
+//			if (numbers[i] == ques)
+//			{
+//				++count;
+//			}
+//		}
+//		if (count > numbers.size() / 2)
+//			return ques;
+//		else
+//			return 0;
+//	}
+//};
+
+//#include <stdio.h>
+//int cnt = 0;
+//int fib(int n)
+//{
+//	cnt++;
+//	if (n == 0)
+//		return 1;
+//	else if (n == 1)
+//		return 2;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//
+//int main()
+//{
+//	fib(8);
+//	printf("%d", cnt);
+//}
+//6.4.1
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
-	string str;
-	getline(cin, str);
-	string tmp, res;
-	for (int i = 0; i < str.size(); ++i)
+	int m1, m2, m3, m4;
+	cin >> m1 >> m2 >> m3 >> m4;
+	int A = (m1 + m3) / 2;
+	int B = (m2 + m4) / 2;
+	int C = (m4 - m2) / 2;
+	if (A - B == m1 && B - C == m2 && A + B == m3 && B + C == m4)
 	{
-		if (str[i] == ' ')
-		{
-			res = ' ' + tmp + res;
-			tmp = "\0";
-		}
-		else
-		{
-			tmp += str[i];
-		}
+		cout << A << " " << B << " " << C << " " << endl;
 	}
-	if (tmp.size())
-	{
-		res = tmp + res;
-	}
-	cout << res << endl;
+	else
+		cout << "NO" << endl;
 	return 0;
 }
+
