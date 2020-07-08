ade488e9c7534817d744c10246b201043f0a38c8
wangxiaohei1005 <18309253051@sina.cn>
2020-07-08 23:57:34 +0800

    7.8

--- 牛客刷题/牛客刷题/question.cpp
+++ 牛客刷题/牛客刷题/question.cpp
@@ -2028,44 +2028,149 @@ int main()
 //	return 0;
 //}
 
+//#include <iostream>
+//#include <math.h>
+//using namespace std;
+//
+//int main()
+//{
+//	int number;
+//	while (cin >> number)
+//	{
+//		cout << number << " = ";
+//		int* number1 = new int[1000];
+//		int j = 0;
+//		for (int i = 2; i <= sqrt(number); i++)
+//		{
+//			while (number % i == 0)
+//			{
+//				if (number != 1)
+//				{
+//					number1[j] = i;
+//					j++;
+//					number /= i;
+//				}
+//			}
+//		}
+//		if (number != 1)
+//		{
+//			number1[j] = number;
+//			j++;
+//		}
+//		for (int k = 0; k < j; k++)
+//		{
+//			cout << number1[k];
+//			if (k + 1 < j)
+//			{
+//				cout << " * ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <cstdio>
+//
+//int main()
+//{
+//	int fib[100001];
+//	fib[0] = 1;
+//	fib[1] = 1;
+//	for (int i = 2; i <= 100000; ++i)
+//	{
+//		fib[i] = fib[i - 1] + fib[i - 2];
+//		fib[i] = fib[i] % 1000000;
+//	}
+//	int n;
+//	while (std::cin >> n)
+//	{
+//		if (n < 29)
+//		{
+//			printf("%d\n", fib[n]);
+//		}
+//		else
+//		{
+//			printf("%06d\n", fib[n]);
+//		}
+//	}
+//	return 0;
+//}
+
+#include <cstdio>
+#include <cmath>
 #include <iostream>
-#include <math.h>
-using namespace std;
+//闰年判断函数
+inline int leap_year(int year)
+{ 
+	return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0); 
+}//足年天数 
+inline int profit_of_year(int year)
+{
+	return 2 * 31 + 1 * 28 + 1 * 31 + 2 * 30 + 1 * 31 + 2 * 30 + 1 * 31 + 2 * 31 + 2 * 30 + 2 * 31 + 1 * 30 + 2 * 31 + leap_year(year);
+}//判断这个月份是不是质数月 
+inline bool prime(int n)
+{
+	return n == 2 || n == 3 || n == 5 || n == 7 || n == 11;
+}//求出一个日子是这一年的第几天 
+int profit_of_this_year(int year, int month, int day)
+{
+	if (!prime(month))
+	{
+		day *= 2;
+	}
+	while (--month)
+	{
+		switch (month)
+		{
+		case 1:
+		case 8:
+		case 10:
+		case 12:
+			day += 62;
+			break;
+		case 3:
+		case 5:
+		case 7:
+			day += 31;
+			break;
+		case 4:
+		case 6:
+		case 9:
+			day += 60;
+			break;
+		case 11:
+			day += 30;
+			break;
+		case 2:
+			day += 28 + leap_year(year);
+			break;
+		default:;
+		}
+	}
+	return day;
+}
 
 int main()
 {
-	int number;
-	while (cin >> number)
+	int year1, month1, day1, year2, month2, day2;
+	int count_profit = 0;
+	while (std::cin >> year1 >> month1 >> day1 >> year2 >> month2 >> day2)
 	{
-		cout << number << " = ";
-		int* number1 = new int[1000];
-		int j = 0;
-		for (int i = 2; i <= sqrt(number); i++)
+		count_profit = 0;
+		count_profit += profit_of_year(year1) -
+			profit_of_this_year(year1, month1, day1 - 1); //这里的day1 - 1虽然有可能会出现0日，但是实际2月0日就相当于1月31日，所以不影响结果。
+		count_profit += profit_of_this_year(year2, month2, day2);
+		if (year1 == year2) //避免起点和终点是同一年，如果是同一年，要减掉这一年的天数。
 		{
-			while (number % i == 0)
-			{
-				if (number != 1)
-				{
-					number1[j] = i;
-					j++;
-					number /= i;
-				}
-			}
+			count_profit -= profit_of_year(year1);
 		}
-		if (number != 1)
+		for (int i = year1 + 1; i < year2; i++) //中间足年每一年的天数 
 		{
-			number1[j] = number;
-			j++;
+			count_profit += profit_of_year(i);
 		}
-		for (int k = 0; k < j; k++)
-		{
-			cout << number1[k];
-			if (k + 1 < j)
-			{
-				cout << " * ";
-			}
-		}
-		cout << endl;
+		std::cout << count_profit << std::endl;
 	}
 	return 0;
 }
