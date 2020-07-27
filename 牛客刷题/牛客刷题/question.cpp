f31e7918fe4224a258a3ba7b5242d0be03c079bc
wangxiaohei1005 <18309253051@sina.cn>
2020-07-27 23:00:50 +0800

    7.27

--- 牛客刷题/牛客刷题/question.cpp
+++ 牛客刷题/牛客刷题/question.cpp
@@ -2711,3 +2711,172 @@ int main()
 //
 //	return 0;
 //}
+
+//#include <vector>
+//
+//using namespace std;
+//class MaxGap 
+//{
+//public:
+//	int findMaxGap(vector<int> A, int n)
+//	{
+//		int max = 0;
+//		int max_n = 0;
+//		for (int i = 0; i < n; i++)
+//		{
+//			if (A[i] > max)
+//			{
+//				max = A[i];
+//			}
+//			max_n = i;
+//		}
+//		if (max_n == 0)
+//		{
+//			return (A[0] - A[n - 1]);
+//		}
+//		if (max_n == n - 1)
+//		{
+//			return (A[n - 1] - A[0]);
+//		}
+//		int Left = (A[max_n] - A[0]);
+//		int Right = (A[max_n] - A[n - 1]);
+//		int tmp = (Left > Right ? Left : Right);
+//		return tmp;
+//	}
+//};
+
+// 问题本身不复杂, 将思路考虑周全即可. 
+#include <vector>
+using namespace std;
+class Printer 
+{ 
+public: 
+	vector<int> clockwisePrint(vector<vector<int> > mat, int n, int m) 
+	{ vector<int> ret; int x1 = 0, y1 = 0; //左上角坐标 
+	int x2 = n-1, y2 = m-1;
+	//右下角坐标 
+	// 这两个坐标表示了一个矩形(最开始的矩阵) 
+	// 然后按照要求打印最外圈的数据. 
+	// 打印完毕之后, 缩小矩形的大小. 
+	while(x1 <= x2 && y1 <= y2)
+	{ 
+		for(int j = y1; j <= y2; ++j) 
+			ret.push_back(mat[x1][j]); 
+		for(int i = x1+1; i < x2; ++i) 
+			ret.push_back(mat[i][y2]);
+		for(int j = y2; x1 < x2 && j >= y1; --j) 
+			//x1 < x2：只有在不是单一的横行时才执行这个循环 
+			ret.push_back(mat[x2][j]); 
+		for(int i = x2-1; y1 < y2 && i > x1; --i)
+			//y1 < y2：只有在不是单一的竖列时才执行这个循环 
+			ret.push_back(mat[i][y1]);
+		x1++; 
+		y1++;
+		x2--;
+		y2--;
+	}
+	return ret;
+	}
+};
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+
+#include "coroutine.h"
+
+int tcp_init() {
+	int lfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ( lfd == -1 ) perror("socket"),exit(1);
+
+	int op = 1;
+	setsockopt(lfd, SOL_SOCKET, SO_REUSEADDR, &op, sizeof(op));
+	
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(9898);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int r = bind(lfd, (struct sockaddr*)&addr, sizeof(addr));
+	if ( r == -1 ) perror("bind"),exit(1);
+	
+	listen(lfd, SOMAXCONN);
+
+	return lfd;
+}
+
+void set_nonblock(int fd) {
+	int flgs = fcntl(fd, F_GETFL, 0);
+	flgs |= O_NONBLOCK;
+	fcntl(fd, F_SETFL, flgs);
+}
+
+void accept_conn(int lfd, schedule_t *s, int co_ids[], void *(*call_back)(schedule_t *s, void *args) ) {
+	while ( 1 ) {
+		int cfd = accept(lfd, NULL, NULL);
+		if ( cfd > 0 ) {
+			set_nonblock(cfd);
+			int args[] = {lfd, cfd};
+			int id = coroutine_create(s, call_back, args);
+			int i;
+			for (i=0; i<CORSZ; i++) {
+				if ( co_ids[i] == -1 )  {
+					co_ids[i] = id;
+					break;
+				}
+			}
+			if ( i == CORSZ ) {
+				printf("连接太多\n");
+			}
+			coroutine_running(s, id);
+		} else {
+			int i;
+			for (i=0; i<CORSZ; i++) {
+				int cid = co_ids[i];
+				if ( cid == -1 ) continue;
+				coroutine_resume(s, cid);
+			}	
+		}
+	}
+}
+
+void *handle(schedule_t *s, void *args) {
+	int *arr = (int*)args;
+	int cfd = arr[1];
+
+	char buf[1024] = {};
+	while ( 1 ) {
+		memset(buf, 0x00, sizeof(buf));
+		int r = read(cfd, buf, 1024);
+		if ( r == -1 )  {
+			coroutine_yield(s);
+		} else if ( r == 0 ) {
+			break;
+		} else {
+			printf("recv:%s\n", buf);
+			if ( strncasecmp(buf, "exit", 4) == 0 ) {
+				break;
+			}
+			write(cfd, buf, r);
+		}
+	}
+}
+
+int main( void ) {
+	int lfd = tcp_init();
+	set_nonblock(lfd);
+
+	schedule_t *s = schedule_create();
+	int co_ids[CORSZ];
+	int i;
+	for (i=0; i<CORSZ; i++)
+		co_ids[i] = -1;
+
+	accept_conn(lfd, s, co_ids, handle);
+	
+	schedule_destroy(s);
+}
